4a0e4387b173311ac4f8f26a506b3dc93bfab0d2
yasinchen <yasinchan2016@gmail.com>
2017-04-08 21:32:27 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 
 ReactDOM.render( <
-    h1 > Hello, world! < /h1>,
-    document.getElementById('root2')
+    App / > ,
+    document.getElementById('root')
 );

04ec3be70fe5933138c834fd9c2badea1d36f0ca
yasinchen <yasinchan2016@gmail.com>
2017-04-14 19:14:02 +0800

    新增 todo

--- src/App.js
+++ src/App.js
@@ -10,27 +10,25 @@
      super(props)
      this.state = {
        newTodo: '',
-       todoList: [
-         {id:1, title:'第一个待办'},
-         {id:2, title:'第二个待办'},
-       ]
+       todoList: []
      }
    }
    render() {
  
      let todos = this.state.todoList.map((item,index)=>{
        return ( // 为什么这里要加个括号？这是动手题3 🐸
-         <li>
+         <li key={index}>
            <TodoItem todo={item} />
          </li>
        )
      })
+     console.log(todos)
  
      return (
        <div className="App">
          <h1>我的待办</h1>
          <div className="inputWrapper">
-           <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
+           <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
          </div>
          <ol>
            {todos}
@@ -38,9 +36,21 @@
        </div>
      )
    }
-   addTodo(){
-     console.log('我得添加一个 todo 了')
+   addTodo(event){
+     this.state.todoList.push({
+       id: idMaker(),
+       title: event.target.value,
+       status: null,
+       deleted: false
+     })
    }
  }
  
  export default App;
+
+ let id = 0
+
+ function idMaker (){
+   id += 1
+   return id
+ }

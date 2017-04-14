2d9fe194ab2ccf056c147f25d90e0e27bdb10bbc
yasinchen <yasinchan2016@gmail.com>
2017-04-14 19:21:58 +0800

    fix value again

--- src/App.js
+++ src/App.js
@@ -1,56 +1,68 @@
-  import React, { Component } from 'react';
-  import 'normalize.css'
-  import './reset.css'
-  import './App.css';
-  import TodoInput from './TodoInput'
-  import TodoItem from './TodoItem'
+ import React, { Component } from 'react';
+ import 'normalize.css'
+ import './reset.css'
+ import './App.css';
+ import TodoInput from './TodoInput'
+ import TodoItem from './TodoItem'
  
  class App extends Component {
    constructor(props){
      super(props)
      this.state = {
        newTodo: '',
-       todoList: []
+       todoList: [
+       ]
      }
    }
    render() {
  
      let todos = this.state.todoList.map((item,index)=>{
        return ( // 为什么这里要加个括号？这是动手题3 🐸
-         <li key={index}>
+         <li key={index} >
            <TodoItem todo={item} />
-         </li>
-       )
+          </li>
+        )
+      })
+  
+      return (
+        <div className="App">
+          <h1>我的待办</h1>
+          <div className="inputWrapper">
+           <TodoInput content={this.state.newTodo} 
+             onChange={this.changeTitle.bind(this)}
+             onSubmit={this.addTodo.bind(this)} />
+          </div>
+          <ol>
+            {todos}
+          </ol>
+        </div>
+      )
+    }
+   changeTitle(event){
+     this.setState({
+       newTodo: event.target.value,
+       todoList: this.state.todoList
      })
-     console.log(todos)
- 
-     return (
-       <div className="App">
-         <h1>我的待办</h1>
-         <div className="inputWrapper">
-           <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
-         </div>
-         <ol>
-           {todos}
-         </ol>
-       </div>
-     )
    }
-   addTodo(event){
-     this.state.todoList.push({
-       id: idMaker(),
+    addTodo(event){
+      this.state.todoList.push({
+        id: idMaker(),
        title: event.target.value,
        status: null,
        deleted: false
+     })
+     this.setState({
+       newTodo: '',
+       todoList: this.state.todoList
      })
    }
  }
  
  export default App;
-
+ 
  let id = 0
-
- function idMaker (){
+ 
+ function idMaker(){
    id += 1
    return id
  }

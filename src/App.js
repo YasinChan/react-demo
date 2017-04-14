962d2338c6a8eabbaaa2f8b3b1df513844097b73
yasinchen <yasinchan2016@gmail.com>
2017-04-14 18:02:25 +0800

    更新样式

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
-import React, { Component } from 'react';
-import 'normalize.css';
-import './reset.css';
-import './App.css';
-import TodoInput from './TodoInput';
-import TodoItem from './TodoItem'
-
-
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      newTodo: 'test',
-      todoList: [
-        {
-          id:1, 
-          title: '第一个代办'
-        },
-        {
-          id:2,
-          title: '第二个代办'
-        }
-      ]
-    }
-  }
-  render() {
-    let todos = this.state.todoList.map((item,index)=>{
-      return ( // 为什么这里要加个括号？这是动手题3 🐸
+  import React, { Component } from 'react';
+  import 'normalize.css'
+  import './reset.css'
+  import './App.css';
+  import TodoInput from './TodoInput'
+  import TodoItem from './TodoItem'
+ 
+ class App extends Component {
+   constructor(props){
+     super(props)
+     this.state = {
+       newTodo: 'test',
+       todoList: [
+         {id:1, title:'第一个待办'},
+         {id:2, title:'第二个待办'},
+       ]
+     }
+   }
+   render() {
+ 
+     let todos = this.state.todoList.map((item,index)=>{
+       return ( // 为什么这里要加个括号？这是动手题3 🐸
          <li>
            <TodoItem todo={item} />
          </li>
        )
-    })
-    return (
-      <div className="App">
-        <h1>我的待办</h1>
-        <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo}/>
-        </div>
-        <ol>
-          {todos}
-        </ol>
-      </div>
-    )
-  }
-}
-
-export default App;
+     })
+ 
+     return (
+       <div className="App">
+         <h1>我的待办</h1>
+         <div className="inputWrapper">
+           <TodoInput content={this.state.newTodo} />
+         </div>
+         <ol>
+           {todos}
+         </ol>
+       </div>
+     )
+   }
+ }
+ 
+ export default App;
